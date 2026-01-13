2d1cc3d36a48fc5a51a05ee0d73a755dcadac650
rbokajel <sabiokajelsikat@gmail.com>
2026-01-14 01:56:50 +0800

    The bar materializes

--- modules/widgets/bar/init.lua
+++ modules/widgets/bar/init.lua
@@ -13,30 +13,33 @@ local launchers = require(widget_module .. "componenets.launcher")
 screen.connect_signal("request::desktop_decoration", function(s) 
     local bar_content = wibox.widget({
         {
-            {
-                {
-                    launchers.fetch_launcher(),  
-                    top = 7,
-                    widget = wibox.container.margin
-                },
-                nil,
-                {
-                    {
-                        { },
-
-                        { },
-                    }
-                    layout = wibox.layout.align.vertical
-                },
-                {
-                    
-                },
-            },
             bg = beautiful.bg_normal,
             fg = beautiful.fg_normal,
             widget = wibox.container.background
         }
     })
+
+    local bar = awful.popup {
+        visible = true,
+        ontop = false,
+        minimum_height = s.geometry.height - beautiful.useless_gap * 4,
+        minimum_width = beautiful.bar_width,
+        bg = beautiful.bg_normal .. "00",
+        fg = beautiful.fg_normal,
+        widget = bar_content,
+        screen = s,
+        placement = function (d)
+            return awful.placement.left(d, {
+                margins = {
+                    left = beautiful.useless_gap * 2
+                }
+            })
+        end
+    }
+
+    bar:struts {
+        left = beautiful.bar_width + beautiful.useless_gap * 2
+    }
     --[[s.mypromptbox = awful.widget.prompt()
     s.mylayoutbox = awful.widget.layoutbox(s)
         s.mylayoutbox:buttons(gears.table.join(
